cfa896e3d3cdc2069056043e872aeaa01ff64a43
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-05 13:42:11 +0300

    feat: use comps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Meta from '@/components/layout/Meta';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import Banner from '@/components/home/Banner';
+import Counters from '@/components/home/Counters';
+import Desc from '@/components/home/Desc';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +13,8 @@ export default function Home(): JSX.Element {
       <Header />
       <main className="m-0 flex flex-col items-center justify-start bg-white p-0 font-theme">
         <Banner />
+        <Counters />
+        <Desc />
       </main>
       <Footer />
     </>
