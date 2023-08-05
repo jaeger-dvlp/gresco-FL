754b44e877e23df7948e3890995dd4627d1d3f9f
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-05 13:44:59 +0300

    rev: meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,19 @@ import Counters from '@/components/home/Counters';
 import Desc from '@/components/home/Desc';
 
 export default function Home(): JSX.Element {
+  /*
+  Gresco is a natural hookah coal brand.
+
+  TR Long & Detail Meta Desc;
+  
+
+  */
   return (
     <>
-      <Meta title=".." description=".." />
+      <Meta
+        title="Gresco"
+        description="Gresco - Doğal Nargile Kömürü Markanız. Eşsiz kalitede ve çevre dostu nargile kömürleriyle benzersiz bir içim deneyimi keşfedin."
+      />
       <Header />
       <main className="m-0 flex flex-col items-center justify-start bg-white p-0 font-theme">
         <Banner />
